ad4bf6ac2fd44f186e9e86018258d4142e8c9cc2
MrPr0per <73241824+MrPr0per@users.noreply.github.com>
2022-12-27 20:09:50 +0500

    Update reset_online.py

--- reset_online.py
+++ reset_online.py
@@ -1,7 +1,19 @@
 import yadisk
 
-yadisk_token = 'your_token'
+yadisk_token = '_'
 
+
+def load_settings():
+    global yadisk_token
+    with open('settings.txt', 'r') as file:
+        for line in file.readlines():
+            var, value = map(lambda x: x.strip(), line.split('='))
+            value = eval(value)
+            if var == 'yadisk_token':
+                yadisk_token = value
+
+
+load_settings()
 ycloud_manager = yadisk.YaDisk(token=yadisk_token)
 print('отправка нового списка на сервер...', end=' ')
 with open('players_online.txt', 'w') as file:
